8f512fea86048182fbdc2245c01b98ca65be952e
Lyra Naeseth <lyra@lyra.codes>
2022-07-01 13:46:04 -0700

    Vendor the direnv install.sh
    On repeated runs, we start getting 403 blocked by GitHub pages. :\

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const core = require('@actions/core');
+const core = require("@actions/core");
 const cp = require("child_process");
+const path = require("path");
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    core.info(`Download direnv ...`)
-    cp.execSync('curl -sfL https://direnv.net/install.sh | bash > /dev/null 2>&1', { encoding: "utf-8" });
+    const install = path.join(__dirname, "install-direnv.sh");
 
-    cp.execSync('direnv allow', { encoding: "utf-8" });
-    const envs = JSON.parse(cp.execSync('direnv export json', { encoding: "utf-8" }));
+    core.info("Installing direnv");
+    cp.execSync(`curl -sfL ${install} | bash > /dev/null 2>&1`, {
+      encoding: "utf-8",
+    });
+
+    cp.execSync("direnv allow", { encoding: "utf-8" });
+    const envs = JSON.parse(
+      cp.execSync("direnv export json", { encoding: "utf-8" })
+    );
 
     Object.keys(envs).forEach(function (name) {
       const value = envs[name];
       core.exportVariable(name, value);
     });
-  }
-  catch (error) {
+  } catch (error) {
     core.setFailed(error.message);
   }
 }
 
-run()
+run();
