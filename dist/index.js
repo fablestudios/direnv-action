3f9f62289be5c3881993dc0134eae54ed17f512f
Lyra Naeseth <lyra@lyra.codes>
2022-08-18 17:49:09 -0700

    Pare down to what we actually use

--- dist/index.js
+++ dist/index.js
@@ -164,38 +164,14 @@ exports.issueCommand = issueCommand;
 
 const core = __webpack_require__(470);
 const cp = __webpack_require__(129);
-const fs = __webpack_require__(747);
-const path = __webpack_require__(622);
 
 // most @actions toolkit packages have async methods
 async function run() {
   try {
-    const install = path.join(__dirname, "install-direnv.sh");
+    const direnv = "asdf exec direnv";
 
-    let direnv = "direnv";
-    let available = false;
-    const asdf = findAsdf();
-
-    if (hasCommand("direnv")) {
-      core.info("direnv is already installed");
-      available = true;
-    } else if (asdf && hasAsdfDirenv(asdf)) {
-      core.info("direnv is already installed (via asdf)");
-      direnv = `${asdf} exec direnv`;
-      available = true;
-    }
-
-    if (!available) {
-      core.info("direnv not found; installing now");
-      cp.execSync(`bash ${install}`, {
-        encoding: "utf-8",
-      });
-    }
-
-    cp.execSync(`${direnv} allow`, { encoding: "utf-8" });
-    const envs = JSON.parse(
-      cp.execSync(`${direnv} export json`, { encoding: "utf-8" })
-    );
+    exec(`${direnv} allow`);
+    const envs = JSON.parse(exec(`${direnv} export json`));
 
     Object.keys(envs).forEach(function (name) {
       const value = envs[name];
@@ -206,30 +182,9 @@ async function run() {
   }
 }
 
-function hasCommand(command) {
-  const { status } = cp.spawnSync(`command -v ${command}`);
-  core.info(
-    `${command} is ${
-      status === 0
-        ? "installed"
-        : `not available on $PATH (${process.env.PATH})`
-    }`
-  );
-  return status === 0;
-}
-
-function findAsdf() {
-  if (hasCommand("asdf")) return "asdf";
-
-  const localAsdf = path.join(process.env.HOME, ".asdf", "bin", "asdf");
-  if (fs.existsSync(localAsdf)) return localAsdf;
-}
-
-function hasAsdfDirenv(asdf) {
-  const { status } = cp.spawnSync(`${asdf} where direnv`, {
-    stdio: ["ignore", "inherit", "inherit"],
-  });
-  return status === 0;
+function exec(command) {
+  core.info(command);
+  return cp.execSync(command, { encoding: "utf-8" });
 }
 
 run();
